0baed605aa53fad25425fda73e2b800e24961f38
bbennett7 <bryn.bennett@gmail.com>
2021-11-06 13:06:00 -0700

    Move code out of scaffolded directories

--- packages/my-app/client/src/App.tsx
+++ packages/my-app/client/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from 'components';
+import { formatters } from 'utils';
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +20,8 @@ function App() {
         >
           Learn React
         </a>
+        <Button label="This is component from a shared library" />
+        <p>{formatters.toSentenceCase('this is text formatted with a shared formatter')}</p>
       </header>
     </div>
   );
